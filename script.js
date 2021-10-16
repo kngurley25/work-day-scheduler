81e18074b6b6d29202f038e4787ffb86cf41e9f2
kngurley <kngurley25@gmail.com>
2021-10-16 14:56:43 -0500

    add audit for time description classes

--- script.js
+++ script.js
@@ -5,55 +5,75 @@ var now = moment().format("MMMM DD, YYYY - hh:mm A");
 $("#currentDay")
     .text(now);
 
-
 // iterate through object of static time periods
 var hourObject = {
-    1: "9:00 AM",
-    2: "10:00 AM",
-    3: "11:00 AM",
-    4: "12:00 PM",
-    5: "1:00 PM",
-    6: "2:00 PM",
-    7: "3:00 PM",
-    8: "4:00 PM",
-    9: "5:00 PM"
+    9: "9:00 AM",
+    10: "10:00 AM",
+    11: "11:00 AM",
+    12: "12:00 PM",
+    13: "1:00 PM",
+    14: "2:00 PM",
+    15: "3:00 PM",
+    16: "4:00 PM",
+    17: "5:00 PM"
 }
+var hourCount = Object.keys(hourObject).length
 
-// function to create dynamic HTML elements
-var createBlock = function(val) {
-    
+// create dynamic HTML elements over each static hour time period
+Object.values(hourObject).forEach(function(key, val) {
+
     var timeBlock = $("<div>")
-        .addClass("row time-block align-items-center");
-    
+    .addClass("row time-block");
+
     var blockHour = $("<div>")
-        .addClass("hour col")
-        .text(val);
-    
-    var blockDescription = $("<p>")
-        .addClass("description col-8")
-        .text("sample description text");
-    
-    var blockBtn = $("<div>") 
-        .addClass("saveBtn col")
-        .text("sample icon");
-    
+    .addClass("hour col")
+    .text(key);
+
+    var blockDescription = $("<textarea>")
+    .addClass("description textarea col-8")
+    .text("");
+
+    var blockBtn = $("<button>") 
+    .addClass("saveBtn far fa-save col")
+
     // append hour div, description div, and save button div to parent row
     timeBlock.append(blockHour, blockDescription, blockBtn);
 
     // append timeblock row to container
     $(".container").append(timeBlock);
-    
-}
 
-// create dynamic HTML elements over each static hour time period
-Object.values(hourObject).forEach(function(val) {
+    // audit description for current time of day
+    var currentHour = moment().hour()
+    console.log("The current hour is " + currentHour);
 
-    createBlock(val);
-
+    var hourValue = val + hourCount;
+    if (hourValue > currentHour) {
+        $(blockDescription).addClass("future");
+    }
+    else if (hourValue === currentHour) {
+        $(blockDescription).addClass("present");
+    }
+    else if (hourValue < currentHour) {
+        $(blockDescription).addClass("past");
+    }
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this isn't needed
 // description text was clicked
-$("description").on("click", "p", function() {
+$("description").on("click", "textarea", function() {
     var text = $(this)
         .text()
         .trim();
@@ -62,9 +82,10 @@ $("description").on("click", "p", function() {
         .val(text);
     $(this).replaceWith(textInput);
 
-    textInput.trigger("focus");
-})
+    schedule[text] = text;
 
+    saveSchedule();
+})
 
 
 
@@ -73,17 +94,14 @@ var loadSchedule = function() {
 
     var schedule = JSON.parse(localStorage.getItem("schedule")) || [];
 
-    // TODO verify this
-    createBlock(schedule);
-
 }
 
 var saveSchedule = function() {
     
-    var schedule = localStorage.setItem("schedule", JSON.stringify("schedule")) || [];
+    var schedule = JSON.parse(localStorage.getItem("schedule")) || [];
 
     var scheduleObj = {
-        // TODO add description part of time block
+        description: blockDescription.text
     }
     schedule.push(scheduleObj);
     localStorage.setItem("schedule", JSON.stringify(schedule));
